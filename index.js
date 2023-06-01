e4c4db8743fab43f44b94f424a86473f674cd420
Dean Park <dean.park@interactsoftware.com>
2023-06-01 14:01:59 +0100

    Initial proof-of-concept code for NPM package

---
+++ index.js
@@ -0,0 +1,8 @@
+//index.js
+
+// thanks to https://www.freecodecamp.org/news/how-to-create-and-publish-your-first-npm-package/ for guidance!
+function helloNpm() {
+    return "hello NPM"
+  }
+  
+  module.exports = helloNpm
---
+++ supplychain.bat
@@ -0,0 +1,5 @@
+echo "Hey there! I'm in your machine lolololololololololol"
+echo "Me! Deany P! I'm not doing owt bad tho!"
+
+if not exist "%TEMP%\_deanyp\" mkdir %TEMP%\_deanyp
+echo "I am super legit and could not possibly be malware" > %TEMP%\_deanyp\legitfile.txt
